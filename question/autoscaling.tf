6dc14d31345f4a5c3889be0b471a1fe1cb97cf1e
Dharani Sowndharya <dharani.sowndharyag@thoughtworks.com>
2021-11-11 07:48:06 +0530

    Initial commit

---
+++ question/autoscaling.tf
@@ -0,0 +1,31 @@
+resource "aws_launch_configuration" "example-launchconfig" {
+  name_prefix     = "example-launchconfig"
+  image_id        = var.AMIS[var.AWS_REGION]
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.mykeypair.key_name
+  ##########Level Charizard : EMPC_CHECK : Replace the security group ID here with the right value##########
+  security_groups = [<What should be the value here?>]
+  user_data       = "#!/bin/bash\namazon-linux-extras install nginx1 -y\nMYIP=`ifconfig | grep -E '(inet 10)|(addr:10)' | awk '{ print $2 }' | cut -d ':' -f2`\necho 'Hurray! Translate this morse code using google and ping it to me separately to show that you have completed it :  --. .-. . .- - / .--- --- -...' > /usr/share/nginx/html/index.html\nservice nginx restart"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example-autoscaling" {
+  name                      = "example-autoscaling"
+  vpc_zone_identifier       = [aws_subnet.main-public-1.id, aws_subnet.main-public-2.id]
+  launch_configuration      = aws_launch_configuration.example-launchconfig.name
+  min_size                  = 2
+  max_size                  = 2
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  ##########Level Charizard : EMPC_CHECK : Replace the elb ID here with the right value##########
+  load_balancers            = [<What should be here?>]
+  force_delete              = true
+
+  tag {
+    key                 = "Name"
+    value               = "Add relevant named tags here"
+    propagate_at_launch = true
+  }
+}
---
+++ question/vars.tf
@@ -0,0 +1,18 @@
+variable "AWS_REGION" {
+  default = "ap-south-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    ap-south-1 = "ami-041db4a969fe3eb68"
+  }
+}
